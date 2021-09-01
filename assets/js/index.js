9783c0db751222d49d337005958b379b56697064
Jamshid91 <61581124+Jamshid91@users.noreply.github.com>
2021-09-01 09:36:05 +0500

    added

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+let faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('showFaqInfo');
+        console.dir(faq.children[0].classList.toggle('activeTitle'))
+    })
+})
